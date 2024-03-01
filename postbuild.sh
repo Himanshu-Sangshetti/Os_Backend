fa731d7bb177810e1776fcb9ffa60d7a34d4cae4
Himanshu-Sangshetti <himanshusangshetty@gmail.com>
2024-03-01 18:07:47 +0530

    Changed postbuild.sh to add node modules

--- postbuild.sh
+++ postbuild.sh
@@ -4,6 +4,8 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute/default
 
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+
 cp server.js ./.amplify-hosting/compute/default
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
