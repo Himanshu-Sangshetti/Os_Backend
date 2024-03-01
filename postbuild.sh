894b86f703c75fbd64b25ba598f0127423ab357b
Himanshu-Sangshetti <himanshusangshetty@gmail.com>
2024-03-01 18:43:00 +0530

    Added subdirectories to the postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -9,3 +9,21 @@ cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp server.js ./.amplify-hosting/compute/default
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+
+
+cp -r ./config ./.amplify-hosting/compute/default/config
+
+cp -r ./controllers ./.amplify-hosting/compute/default/controllers
+
+cp -r ./files ./.amplify-hosting/compute/default/files
+
+cp -r ./helpers ./.amplify-hosting/compute/default/helpers
+
+cp -r ./middlewares ./.amplify-hosting/compute/default/middlewares
+
+cp -r ./model ./.amplify-hosting/compute/default/model
+
+cp -r ./routes ./.amplify-hosting/compute/default/routes
+
+cp -r ./.env ./.amplify-hosting/compute/default/.env
