0b7f01b91b239747183243eb7b19a3734e3a53c4
Himanshu-Sangshetti <himanshusangshetty@gmail.com>
2024-03-02 13:34:59 +0530

    Reduced size further

--- postbuild.sh
+++ postbuild.sh
@@ -4,8 +4,6 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute/default
 
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-
 cp server.js ./.amplify-hosting/compute/default
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
