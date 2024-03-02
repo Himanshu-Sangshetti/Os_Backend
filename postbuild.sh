89d9fc51feccc256fa6956522ae69f80d1da89e5
Himanshu-Sangshetti <himanshusangshetty@gmail.com>
2024-03-02 13:31:29 +0530

    Reduced the size

--- postbuild.sh
+++ postbuild.sh
@@ -14,20 +14,6 @@ cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
 
 cp -r ./config ./.amplify-hosting/compute/default/config
 
-cp -r ./controllers ./.amplify-hosting/compute/default/controllers
-
-cp -r ./files ./.amplify-hosting/compute/default/files
-
-cp -r ./helpers ./.amplify-hosting/compute/default/helpers
-
-cp -r ./middlewares ./.amplify-hosting/compute/default/middlewares
-
-cp -r ./model ./.amplify-hosting/compute/default/model
-
 cp -r ./routes ./.amplify-hosting/compute/default/routes
 
 cp -r ./.env ./.amplify-hosting/compute/default/.env
-
-cp package.json ./.amplify-hosting/package.json
-
-cp package-lock.json ./.amplify-hosting/package-lock.json
